bf9da17d65f29ed47e431068e8613f228f022d7c
Michał Olszewski <miolszewski@outlook.com>
2024-03-15 22:47:24 +0100

    Make GameSession fields listenable

--- lib/model/game_session.dart
+++ lib/model/game_session.dart
@@ -6,29 +6,49 @@ import 'types.dart';
 
 class GameSession extends ChangeNotifier {
   final Maze maze;
-  MazeRoom currentRoom;
+  MazeRoom _currentRoom;
+
+  int visitedRooms = 0;
+  Duration elapsedTime = Duration.zero;
+
+  MazeRoom get currentRoom => _currentRoom;
+
+  set currentRoom(MazeRoom value) {
+    _currentRoom = value;
+    notifyListeners();
+  }
+
   Location get currentLocation => currentRoom.location;
 
-  GameSession.newSession(this.maze) : currentRoom = maze.startingRoom;
+
+  GameSession.newSession(this.maze)
+      : _currentRoom = maze.startingRoom;
 
   void moveThroughLeftDoor() {
     if (currentRoom.hasLeftDoor) {
-      currentRoom = maze.roomsGrid[currentLocation.x][currentLocation.y - 1];
+      currentRoom = maze.roomsGrid[currentLocation.y][currentLocation.x - 1];
+      notifyListeners();
     }
   }
+
   void moveThroughRightDoor() {
     if (currentRoom.hasRightDoor) {
-      currentRoom = maze.roomsGrid[currentLocation.x][currentLocation.y + 1];
+      currentRoom = maze.roomsGrid[currentLocation.y][currentLocation.x + 1];
+      notifyListeners();
     }
   }
+
   void moveThroughTopDoor() {
     if (currentRoom.hasUpperDoor) {
-      currentRoom = maze.roomsGrid[currentLocation.x - 1][currentLocation.y];
+      currentRoom = maze.roomsGrid[currentLocation.y - 1][currentLocation.x];
+      notifyListeners();
     }
   }
+
   void moveThroughBottomDoor() {
     if (currentRoom.hasLowerDoor) {
-      currentRoom = maze.roomsGrid[currentLocation.x + 1][currentLocation.y];
+      currentRoom = maze.roomsGrid[currentLocation.y + 1][currentLocation.x];
+      notifyListeners();
     }
   }
 }
