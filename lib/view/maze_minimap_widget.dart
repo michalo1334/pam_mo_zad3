9bb17ec4885a7f9f754a96714832a97f9cdcd35f
Michał Olszewski <miolszewski@outlook.com>
2024-03-15 22:48:51 +0100

    Implemented buttons for Maze Widgets

--- lib/view/maze_minimap_widget.dart
+++ lib/view/maze_minimap_widget.dart
@@ -20,17 +20,32 @@ class _MazeMinimapWidgetState extends State<MazeMinimapWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return ClipOval(
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: Transform(
-          transform: Matrix4.translation(Vector3(0, 0, 0)),
-          child: GridView.count(
-              crossAxisCount: maze.extent.w,
-            children: maze.rooms.map((e) => MazeRoomMinimapWidget(room: e, gameSession: gameSession)).toList()
+    if(gameSession == null) {
+      return _build();
+    }
+    else {
+      return ListenableBuilder(listenable: gameSession!, builder: (context, child) => _build());
+    }
+  }
+
+  Column _build() {
+    return Column(
+      children: [
+        ElevatedButton(onPressed: () {gameSession?.moveThroughBottomDoor();}, child: Text("W dół")),
+        ElevatedButton(onPressed: () {gameSession?.moveThroughTopDoor();}, child: Text("W góre")),
+        ElevatedButton(onPressed: () {gameSession?.moveThroughLeftDoor();}, child: Text("W lewo")),
+        ElevatedButton(onPressed: () {gameSession?.moveThroughRightDoor();}, child: Text("W prawo")),
+        AspectRatio(
+          aspectRatio: 1,
+          child: Transform(
+            transform: Matrix4.translation(Vector3(0, 0, 0)),
+            child: GridView.count(
+                crossAxisCount: maze.extent.w,
+              children: maze.rooms.map((e) => MazeRoomMinimapWidget(room: e, gameSession: gameSession)).toList()
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
--- lib/view/maze_room_fullscreen_widget.dart
+++ lib/view/maze_room_fullscreen_widget.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+import 'package:pam_mo_zad3/model/game_session.dart';
 
 import '../model/maze_room.dart';
 
 class MazeRoomFullScreenWidget extends StatelessWidget {
   final MazeRoom room;
+  final GameSession? gameSession;
 
   final String gridTemplate = """
   .         upperDoor .
@@ -18,7 +20,8 @@ class MazeRoomFullScreenWidget extends StatelessWidget {
   . . minimap
   """;
 
-  const MazeRoomFullScreenWidget({super.key, required this.room});
+  const MazeRoomFullScreenWidget(
+      {super.key, required this.room, this.gameSession});
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +35,30 @@ class MazeRoomFullScreenWidget extends StatelessWidget {
           columnSizes: [1.fr, 3.fr, 1.fr],
           rowSizes: [1.fr, 3.fr, 1.fr],
           children: [
-            _buildDoor(null, Icons.keyboard_arrow_up).inGridArea("upperDoor"),
-            _buildDoor(null, Icons.keyboard_arrow_right).inGridArea("rightDoor"),
-            _buildDoor(null, Icons.keyboard_arrow_down).inGridArea("lowerDoor"),
-            _buildDoor(null, Icons.keyboard_arrow_left).inGridArea("leftDoor"),
+            _buildDoor(
+                    room.hasUpperDoor
+                        ? () => gameSession?.moveThroughTopDoor()
+                        : null,
+                    Icons.keyboard_arrow_up)
+                .inGridArea("upperDoor"),
+            _buildDoor(
+                    room.hasRightDoor
+                        ? () => gameSession?.moveThroughRightDoor()
+                        : null,
+                    Icons.keyboard_arrow_right)
+                .inGridArea("rightDoor"),
+            _buildDoor(
+                    room.hasLowerDoor
+                        ? () => gameSession?.moveThroughBottomDoor()
+                        : null,
+                    Icons.keyboard_arrow_down)
+                .inGridArea("lowerDoor"),
+            _buildDoor(
+                    room.hasLeftDoor
+                        ? () => gameSession?.moveThroughLeftDoor()
+                        : null,
+                    Icons.keyboard_arrow_left)
+                .inGridArea("leftDoor"),
             _buildCenter().inGridArea("center")
           ],
         ));
@@ -43,7 +66,7 @@ class MazeRoomFullScreenWidget extends StatelessWidget {
 
   Widget _buildDoor(void Function()? cb, IconData icon) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: cb,
       style: ElevatedButton.styleFrom(
           minimumSize: const Size(double.infinity, double.infinity)),
       child: Icon(icon, size: 100),
