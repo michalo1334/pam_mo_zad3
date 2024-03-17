0b85aab8d4003641fa615b272ff96dfd4fad49ff
Michał Olszewski <miolszewski@outlook.com>
2024-03-17 20:09:14 +0100

    Added navigation to score page

--- lib/view/pages/game_page.dart
+++ lib/view/pages/game_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pam_mo_zad3/model/game_session.dart';
 import 'package:pam_mo_zad3/view/maze_room_fullscreen_widget.dart';
 
+import 'score_page.dart';
+
 class GamePage extends StatefulWidget {
   final GameSession gameSession;
 
@@ -15,6 +17,18 @@ class _GamePageState extends State<GamePage> {
   GameSession get gameSession => widget.gameSession;
 
   @override
+  void initState() {
+
+    gameSession.addListener(() {
+      if(gameSession.finished) {
+        _navigateToScorePage();
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -23,8 +37,16 @@ class _GamePageState extends State<GamePage> {
       body: ListenableBuilder(
           listenable: gameSession,
           builder: (context, child) {
-            return MazeRoomFullScreenWidget(room: gameSession.currentRoom, gameSession: gameSession);
+            return MazeRoomFullScreenWidget(
+                room: gameSession.currentRoom, gameSession: gameSession);
           }),
     );
+  }
+
+  void _navigateToScorePage() async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => ScorePage(gameSession: gameSession)));
   }
 }
