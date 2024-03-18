e7ceb5a7f7078e600b320c041bf6c554bb7a7679
Michał Olszewski <miolszewski@outlook.com>
2024-03-18 20:04:38 +0100

    Added field indication if game session ended in a win Minor code cleanup

--- lib/model/game_session.dart
+++ lib/model/game_session.dart
@@ -6,12 +6,19 @@ import 'types.dart';
 
 class GameSession extends ChangeNotifier {
   bool _finished = false;
+  bool _won = false;
+
+  bool get won => _won;
+
+  set won(bool value) {
+    _won = value;
+    notifyListeners();
+  }
 
   bool get finished => _finished;
 
   set finished(bool value) {
     _finished = value;
-    if(_finished) stopwatch.stop();
     notifyListeners();
   }
 
@@ -44,7 +51,7 @@ class GameSession extends ChangeNotifier {
     if (currentRoom.hasLeftDoor) {
       currentRoom = maze.roomsGrid[currentLocation.y][currentLocation.x - 1];
 
-      finished = isInFinalRoom;
+      finishIfInFinalRoom();
       visitedRooms++;
       notifyListeners();
     }
@@ -54,7 +61,7 @@ class GameSession extends ChangeNotifier {
     if (currentRoom.hasRightDoor) {
       currentRoom = maze.roomsGrid[currentLocation.y][currentLocation.x + 1];
 
-      finished = isInFinalRoom;
+      finishIfInFinalRoom();
       visitedRooms++;
       notifyListeners();
     }
@@ -64,7 +71,7 @@ class GameSession extends ChangeNotifier {
     if (currentRoom.hasUpperDoor) {
       currentRoom = maze.roomsGrid[currentLocation.y - 1][currentLocation.x];
 
-      finished = isInFinalRoom;
+      finishIfInFinalRoom();
       visitedRooms++;
       notifyListeners();
     }
@@ -74,13 +81,20 @@ class GameSession extends ChangeNotifier {
     if (currentRoom.hasLowerDoor) {
       currentRoom = maze.roomsGrid[currentLocation.y + 1][currentLocation.x];
 
-      finished = isInFinalRoom;
+      finishIfInFinalRoom();
       visitedRooms++;
       notifyListeners();
     }
   }
 
-  void finishSession() {
+  void finishSession({bool win = true}) {
     finished = true;
+
+    won = win;
+    stopwatch.stop();
+  }
+
+  void finishIfInFinalRoom() {
+    if(isInFinalRoom) finishSession(win: true);
   }
 }
