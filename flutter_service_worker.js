2a988da6f6bf351dccc435e40e77e46b759ccaa2
michalo1334 <michalo1334@users.noreply.github.com>
2024-03-19 15:25:49 +0000

    deploy: 8ec430e78f97cd6d08f1d0ae6a518be9b79b9785

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "415d77a828ef6e7740dd0dc3744fa096",
-"/": "415d77a828ef6e7740dd0dc3744fa096",
+const RESOURCES = {"index.html": "254c64f61282ba63947f32d8666a9caf",
+"/": "254c64f61282ba63947f32d8666a9caf",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
